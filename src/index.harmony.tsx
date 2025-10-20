71611643f36535f246f8e4c475027bb8889c52d6
wuleilei <wuleilei3@h-partners.com>
2025-10-20 16:22:26 +0800

    feat: 适配鸿蒙平台
    Signed-off-by: wuleilei <wuleilei3@h-partners.com>

---
+++ src/index.harmony.tsx
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+*/
+  
+import { Appearance, ColorSchemeName, NativeEventSubscription } from 'react-native';
+import { AppearanceListener } from './Appearance.types';
+
+export class AppearanceHarmony {
+  /**
+   * Note: Although color scheme is available immediately, it may change at any
+   * time. Any rendering logic or styles that depend on this should try to call
+   * this function on every render, rather than caching the value (for example,
+   * using inline styles rather than setting a value in a `StyleSheet`).
+   *
+   * Example: `const colorScheme = Appearance.getColorScheme();`
+   */
+  static getColorScheme(): ColorSchemeName {
+    return Appearance.getColorScheme();
+  }
+
+  /**
+   * Set the color scheme preference. This is useful for overriding the default
+   * color scheme preference for the app. Note that this will not change the
+   * appearance of the system UI, only the appearance of the app.
+   * Only available on iOS 13+ and Android 10+.
+   */
+  static setColorScheme(scheme: ColorSchemeName | null | undefined): void {
+    Appearance.setColorScheme(scheme);
+  }
+
+  /**
+   * Add an event handler that is fired when appearance preferences change.
+   */
+  static addChangeListener(listener: AppearanceListener): NativeEventSubscription {
+    return Appearance.addChangeListener(listener);
+  }
+}
+
+/**
+ * A new useColorScheme hook is provided as the preferred way of accessing
+ * the user's preferred color scheme (e.g. Dark Mode).
+ */
+export function useColorScheme(): ColorSchemeName {
+  return useColorScheme();
+}
